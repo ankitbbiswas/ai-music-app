2bcdda280496d12b07ff6febe4f5a1c68486f42f
Ankit Biswas <105410407+ankitbbiswas@users.noreply.github.com>
2025-02-15 23:56:47 +0000

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,9 @@
-import os
-from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+from .models import Music
+from .serializers import MusicSerializer
 
 class GenerateMusic(APIView):
     def post(self, request):
